ed6585df1197a19ba597974dabd041e26cbd243f
Jan <jan810693@gmail.com>
2024-11-16 16:15:50 +0530

    finding the second largest using two pass

---
+++ Second Largest Element in an Array/twopasssearch.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int getsecondlargest(vector<int> &arr){
+    int n = arr.size();
+
+    int largest = -1, secondlargest = -1;
+    for(int i=0; i<n; i++){
+        if(arr[i]>largest){
+            largest = arr[i];
+        }
+    }
+
+    for(int i=0; i<n; i++){
+        if(arr[i] > secondlargest && arr[i] != largest){
+            secondlargest = arr[i];
+        }
+    }
+    return secondlargest;
+}
+
+int main(){
+    vector<int> arr = {4, 5, 25, 21, 35, 22};
+
+    cout<<getsecondlargest(arr);
+}

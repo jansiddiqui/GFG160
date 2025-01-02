67a940cb0960f0c04c59a5a612356b0b1f5d553d
Jan <jan810693@gmail.com>
2025-01-02 19:55:51 +0530

    Using Hash Map And Prefix Sum

---
+++ DailyProblem/Hashing/Count Subarrays Having Sum K/UsingHashMapAndPrefixSum.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int countSubarrays(vector<int> &arr, int k){
+    unordered_map<int, int> prefixSums;
+
+    int res = 0;
+    int currSum = 0;
+
+    for(int i=0;i<arr.size();i++){
+        currSum += arr[i];
+
+        if(currSum == k){
+            res++;
+        }
+        if(prefixSums.find(currSum - k) != prefixSums.end()){
+            res += prefixSums[currSum - k];
+        }
+    }
+    return res;
+}
+
+int main(){
+    vector<int> arr = {10, 2, -2, -20, 10};
+    int k = -10;
+    cout << countSubarrays(arr, k);
+    return 0;
+}

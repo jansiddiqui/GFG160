65b70c7d68b74f9751ee7837c17bd4675bea8457
Jan <jan810693@gmail.com>
2025-01-17 23:30:59 +0530

    Product of array except self

---
+++ DailyProblem/Prefix Sum/Longest Subarray With Equal Number of 1's And 0's/UsingHashmapAndPrefixSumTechnique.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int maxLen(vector<int> &arr){
+    unordered_map<int, int> mp;
+
+    int preSum = 0;
+    int res = 0;
+
+    for(int i=0;i<arr.size();i++){
+        preSum += (arr[i] == 0) ? -1 : 1;
+
+        if(preSum == 0){
+            res = i + 1;
+        }
+        if (mp.find(preSum) != mp.end()){
+            res = max(res, i - mp[preSum]);
+        }
+        else{
+            mp[preSum] = i;
+        }
+    }
+    return res;
+}

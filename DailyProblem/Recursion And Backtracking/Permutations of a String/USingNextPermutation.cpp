0f151cc48e8b72179aa2a3ffb71a1e728571d6ca
Jan <jan810693@gmail.com>
2025-01-28 20:26:15 +0530

    using nest permutation

---
+++ DailyProblem/Recursion And Backtracking/Permutations of a String/USingNextPermutation.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void permute(string str){
+    sort(str.begin(), str.end());
+    do{
+        cout<<str<<endl;
+    }while(next_permutation(str.begin(), str.end()));
+}
+
+int main(){
+    string str = "JAN";
+    permute(str);
+    return 0;
+}

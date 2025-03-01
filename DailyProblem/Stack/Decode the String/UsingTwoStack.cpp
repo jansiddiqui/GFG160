01ec785196aa097c8a8ca0d3c6c51d004e20fed1
Jan <jan810693@gmail.com>
2025-03-01 20:08:23 +0530

    decoding string using two stack

---
+++ DailyProblem/Stack/Decode the String/UsingTwoStack.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string decodeString(string &s){
+    stack<int> numStack;
+    stack<char> charStack;
+    string temp = "";
+    string res = "";
+
+    for(int i=0;i<s.length();i++){
+        int cnt=0;
+        if(s[i] >= '0' && s[i] <='9'){
+            while(s[i] >= '0' && s[i] <= '9'){
+                cnt = cnt*10 + s[i] - '0';
+                i++;
+            }
+            i--;
+            numStack.push(cnt);
+        }
+        else if(s[i] == ']'){
+            temp = "";
+            cnt = numStack.top();
+            numStack.pop();
+            while(charStack.top() != '['){
+                temp = charStack.top() + temp;
+                charStack.pop();
+            }
+            charStack.pop();
+
+            for(int j=0;j<cnt;j++){
+                res = res.append(temp);
+            }
+
+            for(int j=0;j<res.length();j++){
+                charStack.push(res[j]);
+            }
+
+            res = "";
+        }
+        else{
+            charStack.push(s[i]);
+        }
+    }
+    while(!charStack.empty()){
+        res = charStack.top() + res;
+        charStack.pop();
+    }
+
+    return res;
+}
+
+int main(){
+    string s = "3[b2[ca]]";
+    cout << decodeString(s) <<endl;
+    return 0;
+}

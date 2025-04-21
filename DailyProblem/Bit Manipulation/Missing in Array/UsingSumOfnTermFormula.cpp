5afd72a91e930de2691a4ff47d6da11658a43565
Jan <jan810693@gmail.com>
2025-04-21 09:12:00 +0530

    using sun of n term formula

---
+++ DailyProblem/Bit Manipulation/Missing in Array/UsingSumOfnTermFormula.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int missingNum(vector<int> &arr){
+    int n = arr.size();
+
+    int sum = 0;
+
+    for(int i=0;i<n-1;i++){
+        sum += arr[i];
+    }
+    long long expSum = (n*1LL*(n+1))/2;
+
+    return expSum - sum;
+}
+int main(){
+    vector<int> arr = {1,2,3,4,6,7};
+    cout<<missingNum(arr);
+    return 0;
+}

a3d31099d2cc13161ea1efcb60136a2822ecf927
Jan <jan810693@gmail.com>
2025-04-23 11:01:04 +0530

    using nested loop

---
+++ DailyProblem/Bit Manipulation/Unique Numbers II/NestedLoopFrequencyCounting.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> singleNum(vector<int>& arr){
+    int n = arr.size();
+
+    vector<int> ans;
+
+    for(int i=0;i<n;i++){
+        int cnt = 0;
+        for(int j=0;j<n;j++){
+            if(arr[i] == arr[j]){
+                cnt++;
+            }
+        }
+        if(cnt == 1){
+            ans.push_back(arr[i]);
+        }
+    }
+    sort(ans.begin(), ans.end());
+    return ans;
+}
+int main(){
+    vector<int> arr = {1,2,3,2,1,4};
+    vector<int> ans = singleNum(arr);
+    for(auto i : ans){
+        cout<<i<<" ";
+    }
+    return 0;
+}
